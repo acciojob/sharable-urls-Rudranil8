c4e6a928cedaf2010328f2d38bf85bf1a4e744a2
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-06-17 21:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var x=document.getElementById("url");
-x.event.preventDefault(https://d3dyfaf3iutrxo.cloudfront.net/thumbnail/assignment/question/8bb3b47db47841178b670717ea3c74f9.png);
+x.event.preventDefault("https://d3dyfaf3iutrxo.cloudfront.net/thumbnail/assignment/question/8bb3b47db47841178b670717ea3c74f9.png");
