01f882b98b0a5bc82ac424d8eeda8d2024bdc889
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-06-17 21:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-// your code here
+var x=document.getElementById("url");
+x.event.preventDefault(https://d3dyfaf3iutrxo.cloudfront.net/thumbnail/assignment/question/8bb3b47db47841178b670717ea3c74f9.png);
